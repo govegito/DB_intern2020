af98eecfa346fddec45d5d980ca3b0a360b31b15
Amisha <amishabsingla@gmail.com>
2020-05-19 22:28:41 +0530

    write json file

--- src/main/java/com/maven/spark_sql/App.java
+++ src/main/java/com/maven/spark_sql/App.java
@@ -1,20 +1,13 @@
-package com.maven.spark_sql;
-
-/**
- * Hello world!
- *
- */
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-
+import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args )
     {
-        SparkSession spark = SparkSession.builder().appName("JAVA SQL").master("local")
-				.config("spark.some.config.option", "some-value")
-				.getOrCreate();
+       SparkSession spark = SparkSession.builder().appName("JAVA SQL").master("local")
+				.config("spark.some.config.option", "some-value").getOrCreate();
 		Dataset<Row> df = spark.read().json("employee_set1.json");
 		df.show();
 		
@@ -22,7 +15,13 @@ public class App
 
 		Dataset<Row> sqlDF = spark.sql("SELECT * FROM employee WHERE dept = 'HR' ");
 		sqlDF.show();
-
+		sqlDF.write().json("C:/Users/Administrator/Desktop/output");
+		Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+		String userName = myObj.nextLine();
+		// visit localhost:4040 
+		myObj.close();
 		spark.stop();
+		
+		
     }
 }
