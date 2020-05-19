5a011375499b9ed7515cc4f8d32a83cfe7430315
Amisha <amishabsingla@gmail.com>
2020-05-19 13:27:37 +0530

    new app.js

--- src/main/java/com/maven/spark_sql/App.java
+++ src/main/java/com/maven/spark_sql/App.java
@@ -12,19 +12,17 @@ public class App
 {
     public static void main( String[] args )
     {
-       SparkSession spark = SparkSession.builder().appName("JAVA SQL").master("local")
-				.config("spark.some.config.option", "some-value").getOrCreate();
+        SparkSession spark = SparkSession.builder().appName("JAVA SQL").master("local")
+				.config("spark.some.config.option", "some-value")
+				.getOrCreate();
 		Dataset<Row> df = spark.read().json("employee_set1.json");
 		df.show();
 		
 		df.createOrReplaceTempView("employee");
 
-		Dataset<Row> sqlDF = spark.sql("SELECT empid,empname,dept,salary FROM employee WHERE salary > 15000 and dept = 'HR' ");
+		Dataset<Row> sqlDF = spark.sql("SELECT * FROM employee WHERE dept = 'HR' ");
 		sqlDF.show();
-		
-		Dataset<Row> sqDF = spark.sql("SELECT AVG(salary) AS SUM_SALARY FROM employee WHERE salary > 15000 and dept = 'HR' ");
-		sqDF.show();
-	
+
 		spark.stop();
     }
 }
