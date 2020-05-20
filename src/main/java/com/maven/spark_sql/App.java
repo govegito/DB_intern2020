45cc6ee1514698691dbbc2c0cf2c0c21d27ffaf4
shreya <smodi3178@gmail.com>
2020-05-20 17:44:19 +0530

    src file sql statement update

--- src/main/java/com/maven/spark_sql/App.java
+++ src/main/java/com/maven/spark_sql/App.java
@@ -1,3 +1,4 @@
+package com.maven.spark_sql;
 import java.util.Arrays;
 import java.math.*;
 import java.util.Collections;
@@ -37,6 +38,59 @@ public class App {
 		Dataset<Row> newdata = sqlDF.select("empid", "dept", "salary");
 		newdata.show();
 		newdata.write().json("C:/Users/Administrator/Desktop/output");
+		
+		//CODE STARTS HERE
+				//calculates totalt number of entries (employees)
+				long total_employees = sqlDF.count();
+				System.out.print("The Total Number of Employees are  :  "+ total_employees);
+				//calculating the total salaries 
+				Dataset<Row> data2 = sqlDF.groupBy().sum("salary");
+				data2.show();
+				//median  = total_salaries/total_employees
+				Row median =data2.select("sum(salary)").first();
+				long  sum_salary = median.getLong(0);
+				System.out.print("median is a follows "+ sum_salary);
+				long median_salary = sum_salary/total_employees;
+				System.out.print("Median_Salary for comparision : " + median_salary);
+				
+				// table for employees overpaid with respect to the median Salary
+				Dataset<Row> Overpaid_median = sqlDF.select("empid","dept","job_title").where(sqlDF.col("salary").geq(median_salary));
+				//table for employees underpaid wrt the median salary
+				Dataset<Row> Underpaid_median = sqlDF.select("empid","dept","job_title").where(sqlDF.col("salary").leq(median_salary));
+				
+				System.out.print("\n OverPaid in the Company : \n\n");
+				Overpaid_median.show(); //write it a file
+				Overpaid_median.write().json("C:/Users/Administrator/Desktop/Overpaid");
+				
+				System.out.print("\n UnderPaid in the Company : \n\n");
+				Underpaid_median.show(); //write it to a file
+				Underpaid_median.write().json("C:/Users/Administrator/Desktop/Underpaid");
+				
+				
+				Dataset<Row> Over_data = spark.read().json("C:/Users/Administrator/Desktop/Overpaid");//Reading the Overpaid Dataset 
+				System.out.println("Overpaid Data after Reading it : ");
+				//Sucessfully Read
+				Over_data.show();
+				
+				//dataset sorted by the dept Overpaid who are HR
+				Dataset<Row> Over_data_grp=Over_data.where("dept=='HR'");
+				System.out.print("OverPaid Data grouped by the department HR : ");
+				Over_data_grp.show();
+				Over_data_grp.write().json("C:/Users/Administrator/Desktop/Overpaid_HR");
+				
+				Dataset<Row> Under_data = spark.read().json("C:/Users/Administrator/Desktop/Underpaid"); //Reading the Underpaid Dataset
+				System.out.println("Underpaid Data After Reading : ");
+				Under_data.show();
+				
+				
+				//Underpaid who are IT
+				Dataset<Row> Under_data_grp = Under_data.where("dept=='IT'");
+				System.out.print("Underpaid Data grouped by the Department IT : ");
+				Under_data_grp.show();
+				Under_data_grp.write().json("C:/Users/Administrator/Desktop/Underpaid_IT");
+				
+				
+	
 
 		Scanner myObj = new Scanner(System.in); // Create a Scanner object
 		String userName = myObj.nextLine();
