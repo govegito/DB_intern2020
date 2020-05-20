b0c86194929452059f0ae1fc1d67bb65d223725f
Amisha <amishabsingla@gmail.com>
2020-05-20 12:55:34 +0530

    app.java with sql queries

--- src/main/java/com/maven/spark_sql/App.java
+++ src/main/java/com/maven/spark_sql/App.java
@@ -1,27 +1,47 @@
+import java.util.Arrays;
+import java.math.*;
+import java.util.Collections;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.functions;
+import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import static org.apache.spark.sql.functions.col;
+import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.sql.expressions.UserDefinedFunction;
+import static org.apache.spark.sql.functions.udf;
 import java.util.Scanner;
-public class App 
-{
-    public static void main( String[] args )
-    {
-       SparkSession spark = SparkSession.builder().appName("JAVA SQL").master("local")
+
+public class App {
+	public static void main(String[] args) {
+		SparkSession spark = SparkSession.builder().appName("JAVA SQL").master("local")
 				.config("spark.some.config.option", "some-value").getOrCreate();
 		Dataset<Row> df = spark.read().json("employee_set1.json");
-		df.show();
-		
-		df.createOrReplaceTempView("employee");
+		// df.show();
 
-		Dataset<Row> sqlDF = spark.sql("SELECT * FROM employee WHERE dept = 'HR' ");
-		sqlDF.show();
-		sqlDF.write().json("C:/Users/Administrator/Desktop/output");
-		Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+		// Calculating the net salary
+		spark.sqlContext().udf().register("sums", (Long a, Long b, Long c) -> {
+			return (a + b + c);
+		}, DataTypes.LongType);
+
+		Dataset<Row> sqlDF = df.withColumn("Salary",
+				functions.callUDF("sums", df.col("Effective_annual_compensation.Base_pay"),
+						df.col("Effective_annual_compensation.Bonus"),
+						df.col("Effective_annual_compensation.Commission")));
+
+		sqlDF.sort("Salary").show();
+		// filtering out all those whose years_of_experience is below 14 years
+		sqlDF.filter(col("years_of_experience").geq(14));
+
+		// Creating a new dataset
+		Dataset<Row> newdata = sqlDF.select("empid", "dept", "salary");
+		newdata.show();
+		newdata.write().json("C:/Users/Administrator/Desktop/output");
+
+		Scanner myObj = new Scanner(System.in); // Create a Scanner object
 		String userName = myObj.nextLine();
-		// visit localhost:4040 
 		myObj.close();
 		spark.stop();
-		
-		
-    }
+
+	}
 }
